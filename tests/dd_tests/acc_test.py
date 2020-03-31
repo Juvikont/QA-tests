ae9bd2e1351e62092486f0a14db32b943391d8cc
Yuri Kanapelka <y.kanapelka@gmail.com>
2020-03-31 19:53:35 +0300

    simple class fot tests

--- tests/dd_tests/acc_test.py
+++ tests/dd_tests/acc_test.py
@@ -18,17 +18,6 @@ class Account:
             self.balance *= (1 + interest)
 
 
-my_other_account = Account(1000, "savings")
-
-try:
-    my_other_account.withdraw(1250)
-except ValueError:
-    print("That doesn't work...")
-try:
-    my_other_account.deposit(-100)
-except:
-    print("That doesn't work either...")
-
 my_account = Account(1000, "savings")
 my_account.withdraw(500)
 my_account.deposit(750)
@@ -40,3 +29,22 @@ my_other_account.withdraw(500)
 my_other_account.deposit(750)
 my_other_account.interest(0.05)
 print(my_other_account.balance)
+
+try:
+    my_other_account.withdraw(1250)
+except ValueError:
+    print("That doesn't work...")
+try:
+    my_other_account.deposit(-100)
+except:
+    print("That doesn't work either...")
+
+n = int(input('Enter Number: '))
+for i in range(-n + 1, n):
+    print("   " * (abs(i)), end="")
+    for j in range(n - abs(i), 0, -1):
+        print(j, end="")
+    print("  " * (n - abs(i) - 2), end="")
+    for j in range(2 if abs(i) + 1 == n else 1, n - abs(i) + 1):
+        print(j,end=" ")
+    print("")
