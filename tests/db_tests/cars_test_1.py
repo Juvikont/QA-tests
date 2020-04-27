e355cb873fad222805355e141d9c458cdae637a6
Yuri Kanapelka <y.kanapelka@gmail.com>
2020-04-27 22:33:05 +0300

    db tests

---
+++ tests/db_tests/cars_test_1.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+
+def test_query_database_check_number_of_makes_in_database():
+    conn = sqlite3.connect("cars.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM make")
+    count = cursor.fetchone()
+    print(count)
+    conn.close()
+    assert count[0] == 4
+
+
+def test_query_database_check_number_of_models_for_given_make():
+    conn = sqlite3.connect("cars.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT model.model_name FROM model INNER JOIN "
+        "make ON model.make_id = make.id WHERE make.name = ?",
+        ["Maserati"],
+    )
+    model_name = cursor.fetchone()
+    print(model_name)
+    conn.close()
+    assert model_name[0] == "Quattroporte"
