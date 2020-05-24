1b612d434ab92c9bf2b921de90727a147e5a206b
Yuri Kanapelka <y.kanapelka@gmail.com>
2020-05-24 18:57:49 +0300

    handler

---
+++ tests/selenium_insta/db_handler.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from tests.selenium_insta import constants
+
+
+class DBHandler:
+    def __init__(self):
+        DBHandler.HOST = constants.HOST
+        DBHandler.USER = constants.USER
+        DBHandler.DBNAME = constants.DATABASE
+        DBHandler.PASSWORD = constants.PASS
+
+    HOST = constants.HOST
+    USER = constants.USER
+    DBNAME = constants.DATABASE
+    PASSWORD = constants.PASS
+
+    @staticmethod
+    def get_mydb():
+        if DBHandler.DBNAME == '':
+            constants.init()
+            db = DBHandler()
+            mydb = db.connect()
+            return mydb
+
+    def connect(self):
+        mydb = mysql.connector.connect(
+            host=DBHandler.HOST,
+            user=DBHandler.USER,
+            passwd=DBHandler.PASSWORD,
+            database=DBHandler.DBNAME
+        )
+        return mydb
